1f8d3de6638ac95a8fd3870ef56a2b70a95a5d50
Katiane Santiago <157059745+KatianeSantiago@users.noreply.github.com>
2024-02-21 14:40:07 +0000

    Endpoint `[GET] /item/:id criado`

--- index.js
+++ index.js
@@ -2,11 +2,32 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('Hello World!')
 })
 
 app.get('/oi', function (req, res) {
   res.send('Olá, mundo!')
+})
+
+//Lista de Personagens
+const lista = ['Rick Sanchez', 'Morty Smith', 'Summer Smith']
+
+//Read All --> [GET] / item
+app.get('/item', function(req, res){
+  //Envio a lista inteira como resposta HTTP
+  res.send(lista)
+})
+
+//Read By ID --> [GET] /item/:id
+app.get('/item/:id', function (req, res) {
+  //Acesso o ID no parâmetro de rota
+  const id = req.params.id
+
+  //Acesso item na lista baseado no ID recebido
+  const item = lista [id]
+
+  //Envio o item obtido como resposta HTTP
+  res.send(id)
 })
 
 app.listen(3000)
